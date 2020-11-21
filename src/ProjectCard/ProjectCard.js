4969156982c6bece5d9c96ffc983a724b6e1bd88
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-21 12:49:40 -0700

    Adjust render and remove gifs

--- src/ProjectCard/ProjectCard.js
+++ src/ProjectCard/ProjectCard.js
@@ -6,12 +6,9 @@ const ProjectCard = ({ project }) => {
   return (
     <section className="cardWrap" style={{backgroundImage: 'url(' + project.photo + ')' }}>
       <article className="card">
-        <section className="imageWrap">
-          <img className="gif1" title={`${project.name} app in use`} alt ={`${project.name} app in use`} src={project.gif} className="gif"/>
-          <img className="gif2" title={`${project.name} app in use`} alt ={`${project.name} app in use`} src={project.gif} className="gif"/>
-        </section>
         <section className="infoWrap">
-          <h1 className="cardTitle">{project.name}</h1>
+          <h2 className="cardTitle">{project.name}</h2>
+          <h3 className="status">({project.status})</h3>
           <a href={project.repo} className="projectLinks">
             Repo Link
           </a>
@@ -19,9 +16,20 @@ const ProjectCard = ({ project }) => {
             Deployed Link
           </a>
           <p className="cardDes">{project.long_des}</p>
+          <section className="bullets">
+            <h3 className="tools">Tools :: {project.tools}</h3>
+            <h3 className="tools">Challenge :: {project.challenge}</h3>
+            <h3 className="tools">Purpose :: {project.purpose}</h3>
+            <h3 className="tools">Team :: {project.developers}</h3>
+          </section>
         </section>
       </article>
     </section>
   )
 }
 export default ProjectCard
+
+
+// <section className="imageWrap">
+// <img title={`${project.name} app in use`} alt ={`${project.name} app in use`} src={project.gif} className="gif"/>
+// </section>
