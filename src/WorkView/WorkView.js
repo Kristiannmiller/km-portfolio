b7bbf4127e9755800e76dd65b902e74ef1aa22ed
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-20 21:03:30 -0700

    Add WorkView component

---
+++ src/WorkView/WorkView.js
@@ -0,0 +1,11 @@
+import './WorkView.css';
+import React from 'react';
+
+const WorkView = () => {
+  return (
+    <section className="workWrap">
+      WORK VIEW
+    </section>
+  )
+}
+export default WorkView
