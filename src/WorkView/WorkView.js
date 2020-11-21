216c9ef0970eff655784a774bb01451c4a455fff
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-20 22:04:11 -0700

    Add introduction text to WorkView

--- src/WorkView/WorkView.js
+++ src/WorkView/WorkView.js
@@ -4,7 +4,13 @@ import React from 'react';
 const WorkView = () => {
   return (
     <section className="workWrap">
-      WORK VIEW
+      <h1 className="bigIntro" id="workIntro">Kristi Miller is a visually inspired front-end
+      developer with a background in photojournalism. Based in Denver, Colorado.
+      </h1>
+      <h2 className="scrollPrompt">WORK SAMPLES ↓</h2>
+      <section className="projectCardWrap">
+        
+      </section>
     </section>
   )
 }
