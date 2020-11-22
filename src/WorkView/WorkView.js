f9ab836d76aec35ded34701d417a7c9eb9016c1e
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-22 15:19:33 -0700

    Add key prop to ProjectCard map

--- src/WorkView/WorkView.js
+++ src/WorkView/WorkView.js
@@ -4,14 +4,15 @@ import ProjectCard from '../ProjectCard/ProjectCard.js'
 import projects from '../AppAssets/projectsData.js'
 
 const WorkView = () => {
-  const projectCards = projects.map(app => {
+  const projectCards = projects.map((app, i) => {
     return (
       <ProjectCard
+      key={i}
       project={app}/>
     )
   })
   return (
-    <section className="workWrap">
+    <section data-testid="workWrap" className="workWrap">
       <article className="introWrap">
         <h1 className="bigIntro" id="workIntro">Kristi Miller is a visually inspired front-end
         developer with a background in photojournalism. Based in Denver, Colorado.
