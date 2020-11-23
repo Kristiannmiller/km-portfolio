8526334a2e1a2c2b6a7eaaca3fd5047867552f80
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-23 15:28:21 -0700

    Add title attributes to nav links

--- src/App/App.js
+++ src/App/App.js
@@ -17,35 +17,35 @@ function App() {
       <header className="header">
         <Route>
           <NavLink id="homeNav" className="homeNav" to="/">
-            <img title="Kristi Miller Logo" alt ="Kristi Miller Logo" src={logo} className="logo"/>
+            <img title="Navigate to landing page" alt ="Kristi Miller Logo" src={logo} className="logo"/>
           </NavLink>
         </Route>
         <section className="navigationWrap">
           <Route>
-            <NavLink id="workNav" className="nav" activeClassName="activeNav" to="/projects">
+            <NavLink title="Navigate to Projects page" id="workNav" className="nav" activeClassName="activeNav" to="/projects">
               Projects
             </NavLink>
           </Route>
           <Route>
-            <NavLink id="careerNav" className="nav" activeClassName="activeNav" to="/CV">
+            <NavLink title="Navigate to Career page" id="careerNav" className="nav" activeClassName="activeNav" to="/CV">
               Career
             </NavLink>
           </Route>
           <Route>
-            <NavLink id="photoNav" className="nav" activeClassName="activeNav" to="/Photo">
+            <NavLink title="Navigate to Photography page" id="photoNav" className="nav" activeClassName="activeNav" to="/Photo">
               Photography
             </NavLink>
           </Route>
         </section>
         <section className="socialWrap">
           <a href="https://github.com/Kristiannmiller">
-            <img className="socialLogo" src={github} alt="Click to visit my GitHub page"/>
+            <img title="Find Kristi on Github" className="socialLogo" src={github} alt="Click to visit my GitHub page"/>
           </a>
           <a href="https://www.linkedin.com/in/kristiannmiller/">
-            <img className="socialLogo" src={linkedin} alt="Click to visit my LinkedIn page"/>
+            <img title="Find Kristi on LinkedIn" className="socialLogo" src={linkedin} alt="Click to visit my LinkedIn page"/>
           </a>
           <a href="https://alumni.turing.io/alumni/kristi-miller">
-            <img className="socialLogo" src={turing} alt="Click to visit my Turing portfolio"/>
+            <img title="View Kristi's Turing profile" className="socialLogo" src={turing} alt="Click to visit my Turing portfolio"/>
           </a>
         </section>
       </header>
