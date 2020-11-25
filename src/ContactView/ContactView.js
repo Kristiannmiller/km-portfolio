c254530e4e03e9526ec2bce2722c9a74c821278f
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-24 18:56:43 -0700

    Edit content

--- src/ContactView/ContactView.js
+++ src/ContactView/ContactView.js
@@ -10,8 +10,9 @@ const ContactView = () => {
       <img className="headshot" src={headshot} alt="Kristi Miller with pixie short, rose gold hair and round glasses with greenery in the background"/>
       <article className="contactInfo">
         <h1 className="chatIntro">Let's Chat!</h1>
-        <p>I'm currently looking for work opportunities in Front-End development.</p>
-        <p>Please contact me via email.</p>
+        <p className="workStatus">I'm currently looking for work opportunities
+        <br/> in Front-End development.
+        <br/><br/> Please contact me via email.</p>
         <h2 className="email">kristi.ann.miller@gmail.com</h2>
       </article>
     </section>
