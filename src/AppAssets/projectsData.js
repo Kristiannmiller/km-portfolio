d541ba7884054a8bc4f433ff7fda448a9bb4f645
Kristi Miller <kristi.ann.miller@gmail.com>
2020-12-26 11:38:12 -0700

    Add videos to projectData

--- src/AppAssets/projectsData.js
+++ src/AppAssets/projectsData.js
@@ -11,7 +11,8 @@ const projects = [
   short_des: "An emotion-tracking ruminating experience",
   long_des: "An emotional experience application. Rume was born of an interest in creating a design specific app that helped an already emotionally conscious user do more than simply 'get past' or deescalate feelings, and rather create a 'space' that validates a user where they are at. We at Rume believe that it's just as important to take time and feel what you are feeling, as it is to know how to guide yourself back to peace and calm. A user can enter the site, choose an emotion to best suit their current state, and choose between journaling, feeling (ruminating through animation and sound), or follow a breathing exercise.",
   photo: "https://user-images.githubusercontent.com/65047537/100013665-6fda8880-2d92-11eb-8077-61d34a983c3c.png",
-  gif: "https://raw.githubusercontent.com/nathanielmillard/rume/main/src/Assets/introGif.gif"
+  gif: "https://raw.githubusercontent.com/nathanielmillard/rume/main/src/Assets/introGif.gif",
+  video: "https://www.youtube.com/embed/nlUTWOdFLR8"
   },
   {
   name: "Lyrical Lies",
@@ -25,7 +26,8 @@ const projects = [
   short_des: "A misheard lyrics application for the lyrically challenged people of the world.",
   long_des: "Lyrical lies is made for the lyrically challenged people of the world. The people who belt out songs with confidence, but never actually know the lyrics finally have a place to confess their lyrical transgressions. A user can use the app to search for the song they know they sing the wrong lyrics to. A user can then anonymously post their misheard lyric for the world to see and laugh at mercilessly. The project focuses on key concepts including React, Router, and Asyncronous JavaScript. Robust unit and integration testing are also implemented for each component utilizing the Jest testing suite.",
   photo: "https://raw.githubusercontent.com/Kristiannmiller/lyrical-lies/main/src/Assets/wireframe.png",
-  gif: "https://raw.githubusercontent.com/Kristiannmiller/lyrical-lies/main/src/Assets/Intro.gif"
+  gif: "https://raw.githubusercontent.com/Kristiannmiller/lyrical-lies/main/src/Assets/Intro.gif",
+  video: "https://www.youtube.com/embed/Pgwgn-9hzUs"
   },
   {
   name: "AHOY!",
@@ -39,7 +41,8 @@ const projects = [
   short_des: "A travel tracker and vacation price estimator",
   long_des: "A pirate inspired dream vacation booking application. An application to manage and track different trips for users. Implemented with ES6 syntax and exclusive use of array-prototype methods whenever iterations need to be performed. This application uses Object Oriented Programming to drive the design of the app and the code.",
   photo: "https://raw.githubusercontent.com/Kristiannmiller/km-Travel-Tracker/main/src/images/wireframe.png",
-  gif: "https://raw.githubusercontent.com/Kristiannmiller/km-Travel-Tracker/main/src/images/TTAppTour.gif"
+  gif: "https://raw.githubusercontent.com/Kristiannmiller/km-Travel-Tracker/main/src/images/TTAppTour.gif",
+  video: "https://www.youtube.com/embed/6eic_h8QC0E"
 },
 {
   name: "Jurassic Tac Toe",
@@ -53,7 +56,8 @@ const projects = [
   short_des: "A tic-tac-toe game with Jurassic park inspirations",
   long_des: "Like normal tic tac toe, but with a Jurassic Park twist. This was my initial end-of-mod project for Turing School of Software and Design's Front-End Engineering program. Utilizing my newfound knowledge of Vanilla JavaScript, css, html, and dom manipulation (not to mention a neverending love for Adobe Photoshop), I created this application in 10 days after only 5 weeks of education. A goal of mine was to make the application responsive in mobile views. The goal was met, and the end result was regarded as a high understanding of the subject matter being evaluated.",
   photo: "https://raw.githubusercontent.com/Kristiannmiller/km-tic-tac-toe/master/assets/Wireframe-JTT.png",
-  gif: "https://raw.githubusercontent.com/Kristiannmiller/km-tic-tac-toe/master/assets/DesktopViewTour.gif"
+  gif: "https://raw.githubusercontent.com/Kristiannmiller/km-tic-tac-toe/master/assets/DesktopViewTour.gif",
+  video: "https://www.youtube.com/embed/qLAuGur4Fn0"
 },
 {
   name: "Portfolio Site",
