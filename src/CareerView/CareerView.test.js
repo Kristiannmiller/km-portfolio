31595a63141366cbf3bc90ec64a46a646ceef142
Kristi Miller <kristi.ann.miller@gmail.com>
2021-03-17 10:30:58 -0600

    Add window scroll mock to all necessary test files

--- src/CareerView/CareerView.test.js
+++ src/CareerView/CareerView.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CareerView from './CareerView';
+window.scrollTo = jest.fn();
 
 
 describe('CareerView', () => {
